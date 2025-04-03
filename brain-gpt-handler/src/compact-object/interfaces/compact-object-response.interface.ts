6789cec6bf66f80db905e2c7ebcef5333aa2a404
Kenyon Jared Mora Zamora <kmora16@alumnos.uaq.mx>
2025-04-03 11:44:46 -0600

    feat: add CompactObject module & service

---
+++ brain-gpt-handler/src/compact-object/interfaces/compact-object-response.interface.ts
@@ -0,0 +1,8 @@
+/**
+ * Interface describing the final code snippet returned
+ * to the client for obtaining a compact object.
+ */
+export interface CompactObjectResponse {
+    codeSnippet: string;
+  }
+  
